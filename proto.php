3024b5940e40e666135dfa478a6ff5627b2c91e6
jsabbatier <justine.sabbatier@imerir.com>
2014-11-18 09:21:35 +0100

    Signed-off-by: jsabbatier <justine.sabbatier@imerir.com>

---
+++ proto.php
@@ -0,0 +1,46 @@
+
+<?php
+
+/* Client */
+bool function ajouterClient(Client)
+
+bool function modifierClient(Client)
+
+bool function supprimerClient(Client)
+
+bool function masquerClient(Client)
+
+/* Fournisseur */
+
+bool function ajouterFourniseur(Client)
+
+bool function modifierFourniseur(Client)
+
+bool function supprimerFourniseur(Client)
+
+bool function masquerFourniseur(Client)
+
+/* Employers */
+
+bool function ajouterEmploye(Employe)
+
+bool function modifierEmploye(Employe)
+
+bool function supprimerEmploye(Employe)
+
+bool function masquerEmploye(Employe)
+
+/* Commande */
+
+bool function ajouterCommande(Commande)
+
+bool function modifierCommande(Commande)
+
+bool function supprimerCommande(Commande)
+
+bool function masquerCommande(Commande)
+
+bool function imprimerCommande(Commande)
+
+
+?>
