a2be09d5cc86cda7dfcfb4ef65aaa1ed50a28fdd
Julien Loève <julien.loeve@imerir.com>
2014-11-21 18:15:28 +0100

    Signed-off-by: Julien Loève <julien.loeve@imerir.com>

--- Serveur HTTP/get_clients.php
+++ Serveur HTTP/get_clients.php
@@ -1,10 +1,15 @@
 <?php
 require_once("../../../../mdacosta/www/pima3a/classes/daoClient.php");
+require_once("../../../../mdacosta/www/pima3a/classes/daoAdresse.php");
 require_once("../../../../mdacosta/www/pima3a/connect.php");
 
 $objClient = new DaoClient();
 $client = new Client();
 $liste_client = Array();
+
+$objAdresse = new DaoAdresse();
+$adresse = new Adresse();
+$liste_adresse = Array();
 
 $liste_client = $objClient -> getListeClients();
 $retour = Array();
@@ -19,6 +24,19 @@ foreach($liste_client as $tmp)
 												Array("raison" => $client -> getRaison()),
 												Array("commercial" => $client -> getIdCommercial()),
 												Array("etat" => $client -> getEtat()));
+	$adresse = new Adresse();		
+	$liste_adresse = $objAdresse -> getAdresseByIdClient($client -> getIdClient());
+	foreach($liste_adresse as $adresse)
+	{
+		if ($adresse["nom"] == "facturation")
+		{
+			$retour[$client -> getIdClient()]["adresse_f"] = $adresse["adresse"];
+		}
+		else if
+		{
+			$retour[$client -> getIdClient()]["adresse_l"] = $adresse["adresse"];
+		}
+	}
 }
 header("HTTP/1.1 200 OK");
 echo json_encode($retour);
--- Serveur HTTP/get_fournisseurs.php
+++ Serveur HTTP/get_fournisseurs.php
@@ -1,19 +1,26 @@
 <?php
-require_once("../dao/classes/daoFournisseur.php");
-require_once("../dao/connect.php");
+require_once("../../../../mdacosta/www/pima3a/classes/daoFournisseur.php");
+require_once("../../../../mdacosta/www/pima3a/connect.php");
 
 $objFournisseur = new DaoFournisseur();
 $fournisseur = new Fournisseur();
-$liste_fournisseur = Array();
+$liste_Fournisseur = Array();
 
-$liste_fournisseur = $objFournisseur -> getListeFournisseur();
+$liste_fournisseur = $objFournisseur -> getListeFournisseurs();
 $retour = Array();
 
 foreach($liste_fournisseur as $tmp)
 {
 	$fournisseur = $tmp;
-	$retour[$fournisseur -> getIdFournisseur()] = $fournisseur -> getNomFournisseur();
+	$retour[$fournisseur -> getIdFournisseur()] = Array(	Array("nom" => $fournisseur -> getNomFournisseur()),
+															Array("adresse" => $fournisseur -> getAdresseFournisseur()),
+															Array("telephone" => $fournisseur -> getTelephoneFournisseur()),
+															Array("fax" => $fournisseur -> getFaxFournisseur()),
+															Array("contact" => $fournisseur -> getContactFournisseur()),
+															Array("mail" => $fournisseur -> getEmailContactFournisseur()),
+															Array("raison" => $fournisseur -> getRaisonFournisseur()),
+															Array("etat" => $fournisseur -> getEtatFournisseur()));
 }
-
+header("HTTP/1.1 200 OK");
 echo json_encode($retour);
 ?>
