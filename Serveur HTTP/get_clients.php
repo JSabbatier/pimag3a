4e5d8a17e663820dc44656f608dc4294b23a387d
Julien Loève <julien.loeve@imerir.com>
2014-11-21 17:30:57 +0100

    Signed-off-by: Julien Loève <julien.loeve@imerir.com>

--- Serveur HTTP/get_clients.php
+++ Serveur HTTP/get_clients.php
@@ -6,16 +6,19 @@ $objClient = new DaoClient();
 $client = new Client();
 $liste_client = Array();
 
-$liste_client = $objClient -> getListeClient();
+$liste_client = $objClient -> getListeClients();
 $retour = Array();
 
 foreach($liste_client as $tmp)
 {
 	$client = $tmp;
 	$retour[$client -> getIdClient()] = Array(	Array("nom" => $client -> getNomClient()),
-												Array("numero"=> $client -> getNumeroClient()),
-												Array("contact"=> $client -> getContactClient()),
-												;
+												Array("numero" => $client -> getNumeroTel()),
+												Array("contact" => $client -> getNomContact()),
+												Array("mail" => $client -> getEmailContact()),
+												Array("raison" => $client -> getRaison()),
+												Array("commercial" => $client -> getIdCommercial()),
+												Array("etat" => $client -> getEtat()));
 }
 header("HTTP/1.1 200 OK");
 echo json_encode($retour);
