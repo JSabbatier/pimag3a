c332abea26eeaa9c5e9550f4947952b9a6b58657
jsabbatier <justine.sabbatier@imerir.com>
2014-11-25 13:57:35 +0100

    Signed-off-by: jsabbatier <justine.sabbatier@imerir.com>

--- client/footer.php
+++ client/footer.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Copyright &copy; BAD MONKEY 2014</p>
                 </div>
             </div>
         </div>
